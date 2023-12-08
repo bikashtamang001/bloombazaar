8ee0f0f65ec2a85e1b34bbc57b3b2f86630cfce6
bikash_tamang <bikashtmzz@gmail.com>
2023-12-08 15:27:36 +0545

    first commit

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,8 @@
+
+    <div class="sidebar">
+        <a class="active" href="/nursery_online/admin">Home</a>
+        <a href="product.php">Add Product</a>
+        <a href="products.php">All Products</a>
+        <a href="order.php">Customer Orders</a>
+        <a href="users.php">All Customers</a>
+    </div>
